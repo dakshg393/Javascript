12555e6391243c07cd7a58403f808498bcdf5b91
Daksh Gupta <142725301+dakshg393@users.noreply.github.com>
2024-09-08 11:21:37 +0000

    commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,9 @@
-const account id
+const accountId = 14553
+let accountEmail = "daksh@gmail.com"
+accountcity = "Indore"
+let accountDate;
+// accountId = 2 not allowed because of const keyword
+console.log(accountId);
+console.table([accountId, accountEmail,accountcity,accountDate])
+
+console.log(3030+33)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null => istype of object
+// Symbol => has unique vlaue means u mathch both symbole value and it will false because symbol have unique
+// Object
+
+let a= "hello"
+let b= 1
+let c= BigInt(2222222222222411212)
+let d= true
+let e;
+let f = null
+let g = Symbol("hello")
+let h = Symbol("hello")
+let i = ['az',2,'ad',"sdd"]
+
+console.table([a,b,c,d,e,f,g,h,i])
